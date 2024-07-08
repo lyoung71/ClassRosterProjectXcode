0e997876e9ae9224c57ce2c1aa4fcd3756db6261
Landon <landonyoung71@gmail.com>
2024-07-08 15:53:26 -0300

    Initial Commit

---
+++ ClassRosterProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ClassRosterProject
+//
+//  Created by Landon Young on 8/7/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
